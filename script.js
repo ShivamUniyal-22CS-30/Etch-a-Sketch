c5a4936b65e7cfce6ea6b4db7ba8889544a9aa3e
Shivam Uniyal <shivamuniyal1810@gmail.com>
2024-02-13 22:14:39 +0530

    JS code for randomRGB button

--- script.js
+++ script.js
@@ -4,30 +4,46 @@ document.addEventListener('DOMContentLoaded',()=>{
     const containerDiv = document.querySelector('#container');  
     
     function makeGrids(size){
-    for(let rowIndex = 0; rowIndex < size; rowIndex++){
-        let row = document.createElement('div');
-        row.classList.add('gridRow');
-        // row.style.border = "2px solid red";
+        for(let rowIndex = 0; rowIndex < size; rowIndex++){
+            let row = document.createElement('div');
+            row.classList.add('gridRow');
+            // row.style.border = "2px solid red";
 
-        for(let colIndex = 0; colIndex<size; colIndex++){
-            let colCell = document.createElement('div');
-            colCell.classList.add('gridItem');
-            // colCell.style.border = "1px solid gray";
-            // colCell.innerText = "16";
+            for(let colIndex = 0; colIndex<size; colIndex++){
+                let colCell = document.createElement('div');
+                colCell.classList.add('gridItem');
+                // colCell.style.border = "1px solid gray";
+                // colCell.innerText = "16";
 
-            row.appendChild(colCell);
+                row.appendChild(colCell);
+            }
+            containerDiv.appendChild(row);
+            
         }
-        containerDiv.appendChild(row);
+        // HOVERING COLOR
+        const gridItems = document.querySelectorAll('.gridItem');
         
-    }
-    // HOVERING COLOR
-    const gridItems = document.querySelectorAll('.gridItem');
-    
-    gridItems.forEach(function(gridItem){
-        gridItem.addEventListener('mouseover',() => {
-            gridItem.style.backgroundColor = 'pink';
+        gridItems.forEach(function(gridItem){
+            gridItem.addEventListener('mouseover',() => {
+                gridItem.style.backgroundColor = 'brown';
+            });
         });
-    });
+
+        // RANDOMrgb BACKGROUND TO GRIDITEM
+        const randomRGB = document.querySelector('.randomRGB');
+
+        randomRGB.addEventListener('click',() => {
+        gridItems.forEach((gridItem) => {
+            gridItem.addEventListener('mouseover',() => {
+                // Math.random() for random RGB
+                let rgb1 = Math.round(Math.random() * 255 + 1);
+                let rgb2 = Math.round(Math.random() * 255 + 1);
+                let rgb3 = Math.round(Math.random() * 255 + 1);
+
+                gridItem.style.backgroundColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})` ;
+            })
+        })
+});
     
 }
 
